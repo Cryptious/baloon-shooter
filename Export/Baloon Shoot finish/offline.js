1fbd4dbe0cab9849414dd566686baf3f861c8a2a
Martino Wagey <martinowagey@gmail.com>
2018-11-16 14:45:33 +0700

    Add File Project

---
+++ Export/Baloon Shoot finish/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1541499121,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/baloon-sheet0.png",
+		"images/karakter-sheet0.png",
+		"images/peluru-sheet0.png",
+		"images/ledakan.png",
+		"images/backgroundbygreggman-sheet0.png",
+		"images/tali16x-sheet0.png",
+		"images/tali.png",
+		"images/logodacorenew-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
